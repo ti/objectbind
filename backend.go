113592def795bb3a2506658db395b885ad52551b
Nanxi <leenanxi@gmail.com>
2020-01-19 18:05:53 +0800

    Update backend.go

--- backend.go
+++ backend.go
@@ -9,6 +9,13 @@ import (
 	"github.com/ti/objectbind/file"
 )
 
+// Backend to support additional backends, such as etcd, consul, file ...
+type Backend interface {
+	Load(ctx context.Context, path string) (data map[string][]byte, err error)
+	Save(ctx context.Context, path string, data []byte) error
+	Watch(ctx context.Context, paths []string, onChange func(map[string][]byte)) error
+}
+
 // NewBackend new backend
 type NewBackend func(ctx context.Context, uri *url.URL) (Backend, error)
 
@@ -24,13 +31,7 @@ func SetBackend(scheme string, backend NewBackend) {
 	backends[scheme] = backend
 }
 
-// Backend to support additional backends, such as etcd, consul, file ...
-type Backend interface {
-	Load(ctx context.Context, path string) (data map[string][]byte, err error)
-	Save(ctx context.Context, path string, data []byte) error
-	Watch(ctx context.Context, paths []string, onChange func(map[string][]byte)) error
-}
-
+// init load etcd and file to default backend
 func init() {
 	SetBackend(schemeFile, func(ctx context.Context, uri *url.URL) (Backend, error) {
 		return file.New(ctx, uri)
