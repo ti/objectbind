9b7192021966f5818ac89c8759477416bb906cdd
Nanxi <leenanxi@gmail.com>
2021-03-09 11:16:33 +0800

    uprade etcd v3 package

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/ti/objectbind"
-	"go.etcd.io/etcd/v3/clientv3"
-	"go.etcd.io/etcd/v3/pkg/transport"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/pkg/v3/transport"
 )
 
 const schemeEtcd = "etcd"
